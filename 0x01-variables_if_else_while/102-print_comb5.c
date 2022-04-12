71ca3dfefe7f10b233d53c80e32a29b6f5ae2dd0
Bholuh <bholuh06@gmail.com>
2022-04-12 15:13:47 -0700

    Task 12 C variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,21 +14,21 @@ for (num1 = 0; num1 <=98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-putcher((num1 / 10) + '0');
-putcher((num1 % 10) + '0');
-putcher(' ');
-putcher((num2 / 10) + '0');
-putcher((num2 % 10) + '0');
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
 continue;
 
-putcher(',');
-putcher(' ');
+putchar(',');
+putchar(' ');
 }
 }
 
-putcher('\n');
+putchar('\n');
 
 return (0);
 }
