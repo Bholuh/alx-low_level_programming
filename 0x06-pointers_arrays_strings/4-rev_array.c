e9b1df42439a075eb57241ba718bd706adf850a9
Bholuh <bholuh06@gmail.com>
2022-04-24 08:47:53 -0700

    Task 4 C more pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,16 +12,22 @@ void reverse_array(int *a, int n)
 int *start_d, *end_d d;
 int e;
 
+
 start_d = a;
 end_d = a;
+
 
 for (e = 0; e < n - 1; e++)
 {
 end_d++;
 }
 
+
+
+
 for (e = 0; e < n / 2; e++)
 {
+
 
 d = *end_c;
 *end_d = *start_d;
