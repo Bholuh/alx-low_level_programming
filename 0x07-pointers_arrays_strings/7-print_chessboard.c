e196fea912fc8112b791ca5407aa35e77854cfb3
Bholuh <bholuh06@gmail.com>
2022-04-27 04:33:59 -0700

    Task 6 C even more pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ *Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+/*Declaring variables*/
+int a, b;
+
+a = 0;
+while (a < 8) /*number repetitions*/
+{
+b = 0;
+while (b < 8)
+{
+_putchar (a[a][b]);
+b++;
+}
+_putchar ('\n'); /*print new line*/
+a++; /*add +1*/
+}
+
+}
