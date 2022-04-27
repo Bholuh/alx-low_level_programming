f47e7e9e19e408315fbfcd026a0c693fc3a902ba
Bholuh <bholuh06@gmail.com>
2022-04-27 04:55:59 -0700

    Task 7 C even more pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,20 +11,20 @@
 void print_diagsums(int *a, int size)
 {
 /*Declaring variables*/
-int j;
+int i;
 int sum1, sum2;
 
 sum1 = 0;
 sum2 = 0;
 
-j = 0;
-while (j < size) /*number repetitions*/
+i = 0;
+while (i < size) /*number repetitions*/
 {
 /*sums for diagsums*/
-sum1 = sum1 + *(a + j * size + j);
-sum2 = sum2 + *(a + j * size + size - j - 1);
+sum1 = sum1 + *(a + i * size + i);
+sum2 = sum2 + *(a + i * size + size - i - 1);
 
-j++; /*add +1*/
+i++; /*add +1*/
 }
 
 printf("%i, %i\n", sum1, sum2);
